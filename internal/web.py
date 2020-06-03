141b4af45bf8401296bede0d4aa54da8f9800966
Philemon Heng <phil.hsy@outlook.com>
2020-06-03 21:43:20 +0800

    added map ethiopia

--- internal/web.py
+++ internal/web.py
@@ -57,6 +57,7 @@ from handlers import turkey
 from handlers import cambodia
 from handlers import andorra
 from handlers import egypt
+from handlers import ethiopia
 # ---addmap.py header marker---
 # !!!END DO NOT MODFIY
 
@@ -157,6 +158,7 @@ cartogram_handlers = {
 'cambodia': cambodia.CartogramHandler(),
 'andorra': andorra.CartogramHandler(),
 'egypt': egypt.CartogramHandler(),
+'ethiopia': ethiopia.CartogramHandler(),
 # ---addmap.py body marker---
 # !!!END DO NOT MODFIY
 }
